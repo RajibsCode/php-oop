b83c92afd6c3d67cf22417b9d0ba917f9378b353
rajibscode <rajibsarder321@gmail.com>
2022-08-14 23:02:16 +0600

    complete 48%

---
+++ 16final-keyword/index.php
@@ -0,0 +1,21 @@
+<?php
+
+
+final class A {
+    final public function maths($a,$b) {
+        echo "addition is ". $a + $b;
+    }
+}
+
+class B extends A {
+    
+    public function maths_test($a, $b) {
+        echo "Multiplication is: " . $a * $b;
+    }
+}
+
+
+$obj = new B;
+$obj->maths_test(5,5);
+
+?>
